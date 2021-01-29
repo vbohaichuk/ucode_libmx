05f6e4abfcf3b73b3f8b67c330c7f949d59761b0
Валентин Богайчук <vbohaichuk@e1r2p6.unit.ua>
2021-01-29 13:18:46 +0200

    libmx

---
+++ mx_get_substr_index.c
@@ -0,0 +1,21 @@
+#include "libmx.h"
+
+int mx_get_substr_index(const char *str, const char *sub) {
+    int ind = 0;
+    int sublen = 0;
+
+    if (sub && str) {
+        sublen = mx_strlen(sub);
+        while (*str) {
+            if (mx_strncmp(str, sub, sublen) == 0)
+                return ind;
+            ind++;
+            str++;
+        }
+        return -1;
+    }
+    return -2;
+}
+int main() {
+    mx_printint(mx_get_substr_index(NULL,NULL));
+}
